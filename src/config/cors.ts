b1bc6557c0bf6b6ddf004378803b3bb909b4226b
devpiush <dev.bosepiush@gmail.com>
2024-12-15 13:15:43 +0530

    code: added cocrs origin for both web and admin dashboard

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,10 @@ import cors from "cors";
 import { getEnv } from "./env";
 
 export const corsOptions = {
-  origin: getEnv<string>("CORS_ORIGIN", "http://localhost:5173"),
+  origin: [
+    getEnv<string>("CORS_ORIGIN_ADMIN", "http://localhost:5173"),
+    getEnv<string>("CORS_ORIGIN_WEB", "http://localhost:5173"),
+  ],
   credentials: true,
 };
 
