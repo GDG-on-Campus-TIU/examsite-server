1a0d20ce737b4451c24f12cac8c5bc70de68e31b
Arnab Chatterjee <arnabchatterjee912@gmail.com>
2024-12-15 14:57:19 +0530

    ss

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,7 +3,8 @@ import { getEnv } from "./env";
 
 export const corsOptions = {
   origin: [
-    getEnv<string>("CORS_ORIGIN_ADMIN", "http://localhost:5173"),
+    getEnv<string>("CORS_ORIGIN_ADMIN_LOCAL", "http://localhost:5173"),
+    getEnv<string>("CORS_ORIGIN_ADMIN", "https://gdgexams-admin.netlify.app"),
     getEnv<string>("CORS_ORIGIN_WEB", "http://localhost:5173"),
   ],
   credentials: true,
